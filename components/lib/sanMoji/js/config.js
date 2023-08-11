73bad043fa98b9ec8ba4caccda1944329d3b65c1
yas vanny <64766541+vanny-y@users.noreply.github.com>
2023-08-11 17:14:34 +0300

    Delete components directory

--- components/lib/sanMoji/js/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    _napi: 'AIzaSyACoSPkNe8hlg34Ket7uryZMInHCYV4OHI',
-    _ndomain: 'nimo-app.firebaseapp.com',
-    _pid: 'nimo-app',
-    _aid: '1:494981267177:web:bbf9a92c2e798c95d77a78',
-    _stBck: "nimo-app.appspot.com",
-}
-
-export default config;
